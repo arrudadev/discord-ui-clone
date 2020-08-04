c24f57b94d70f324df22bcc17ce4d4d06a1477e7
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2020-08-04 00:34:18 -0300

    chore: add userInfo component

---
+++ src/components/UserInfo/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import {
+  Container,
+  Profile,
+  Avatar,
+  UserData,
+  Icons,
+  MicIcon,
+  HeadphoneIcon,
+  SettingsIcon,
+} from './styles';
+
+const UserInfo: React.FC = () => {
+  return (
+    <Container>
+      <Profile>
+        <Avatar />
+
+        <UserData>
+          <strong>User Name</strong>
+          <span>#4564</span>
+        </UserData>
+      </Profile>
+
+      <Icons>
+        <MicIcon />
+        <HeadphoneIcon />
+        <SettingsIcon />
+      </Icons>
+    </Container>
+  );
+};
+
+export default UserInfo;
